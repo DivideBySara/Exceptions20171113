2834c45f1466d1c9cb04cb6e76df6dbfc27f48f3
Sara Jade <v-sajade@microsoft.com>
2017-11-13 11:24:27 -0800

    Update Program.cs for exception handling + a method that generates exceptions.

--- Exceptions20171113/Program.cs
+++ Exceptions20171113/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Console;
 
 namespace Exceptions20171113
 {
@@ -10,6 +11,48 @@ namespace Exceptions20171113
     {
         static void Main(string[] args)
         {
+            int[] values = { 0, 1, 2, 3, 4, 5 };
+
+            try
+            {
+                int i = GetValueAtIndex(values, 100);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            catch (IndexOutOfRangeException)
+            {
+                WriteLine("Invalid index");
+            }
+            catch (ArgumentNullException ex)
+            {
+                WriteLine(ex.Message);
+            }
+            catch (NullReferenceException)
+            {
+                WriteLine("Please gimme a valid array");
+            }
+            catch (Exception ex)
+            {
+                WriteLine("Not a NullReference or IndexOutOfRangeException." 
+                    + ex.Message);
+            }
+
+            ReadKey();
+        } // End Main()
+
+        private static int GetValueAtIndex(int[] values, int v)
+        {
+            if (values == null) throw new ArgumentNullException
+                    ("Array cannot be null");
+            if (v < 0 || v > values.Length)
+            {
+                throw new ArgumentOutOfRangeException
+                    ("Index must be greater than 0 and less than array length");
+            } // else v is within range
+                
+            return values[v];
         }
     }
 }
